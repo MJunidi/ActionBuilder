c7d7cedf3a2ec1e3839fabc8a7682e6f5ca5240e
MJunidi <m.juneidi@labiba.ai>
2024-10-16 13:36:44 +0300

    test changes

--- ActionBuilder/Controllers/HomeController.cs
+++ ActionBuilder/Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace ActionBuilder.Controllers
         {
             return View();
         }
+        
+        public IActionResult TEST()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
