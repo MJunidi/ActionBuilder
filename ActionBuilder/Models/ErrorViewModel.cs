976759f76d3d7675becc2fbdcbd41d8bfa4a7718
MJunidi <m.juneidi@labiba.ai>
2024-10-17 09:09:20 +0300

    changes

--- ActionBuilder/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ActionBuilder.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
